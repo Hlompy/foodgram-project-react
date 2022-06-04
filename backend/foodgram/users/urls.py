d28b3e6a60d91ae7baac589e07936de0b218ef6c
George Kryukov <k.viregor@gmail.com>
2022-06-05 01:19:24 +1100

    jwt

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CustomUserViewSet, FollowListView, FollowViewSet
+from .views import (UserViewSet, UserSignupViewSet, UserJSWTokenViewSet,
+                    FollowListView, FollowViewSet)
 
-router = DefaultRouter()
-router.register('users', CustomUserViewSet, basename='users')
+router_v1 = DefaultRouter()
+router_v1.register('users', UserViewSet, basename='users')
+router_v1.register('auth/signup', UserSignupViewSet)
+router_v1.register('auth/token', UserJSWTokenViewSet)
 
 urlpatterns = [
     path(
@@ -17,7 +20,5 @@ urlpatterns = [
         FollowViewSet.as_view(),
         name='subscribe'
     ),
-    path('', include(router.urls)),
-    path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('v1/', include(router_v1.urls)),
 ]
--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,27 +1,95 @@
-from djoser.views import UserViewSet
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from rest_framework import status
+from rest_framework import filters, generics, status, viewsets
 from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
+from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
+from api.permissions import IsAdminPermission
+from foodgram.settings import EMAIL_ADMIN
 from .models import User, Follow
-from .serializers import FollowSerializer, CustomUserSerializer
+from .serializers import (SignupSerializer, TokenSerializer,
+                          UserSerializer, FollowSerializer)
 
 
-class CustomUserViewSet(UserViewSet):
+class UserSignupViewSet(viewsets.ViewSetMixin, generics.CreateAPIView):
+    """Регистрация пользователя."""
     queryset = User.objects.all()
-    serializer_class = CustomUserSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = (AllowAny,)
+
+    def create(self, request):
+        serializer = SignupSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = self.request.data.get('username')
+        email = self.request.data.get('email')
+        user = serializer.save(username=username, email=email)
+        confirmation_code = default_token_generator.make_token(user)
+        send_mail("Token", confirmation_code, EMAIL_ADMIN, (email,),)
+        return Response(
+            {'email': email, 'username': username},
+            status=status.HTTP_200_OK
+        )
+
+
+class UserJSWTokenViewSet(viewsets.ViewSetMixin, generics.CreateAPIView):
+    """Получение JSW-токена."""
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def create(self, request):
+        serializer = TokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.data['username']
+        confirmation_code = serializer.data['confirmation_code']
+        user = get_object_or_404(User, username=username)
+        if default_token_generator.check_token(user, confirmation_code):
+            jwt_token = RefreshToken.for_user(user)
+            return Response(
+                {'Token': str(jwt_token.access_token)},
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """Работа с пользователями."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminPermission,)
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
+    lookup_field = 'username'
+
+    @action(
+        detail=False,
+        methods=['GET', 'PUT', 'PATCH'],
+        permission_classes=(IsAuthenticated,)
+    )
+    def me(self, request):
+        serializer = UserSerializer(
+            request.user,
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save(role=request.user.role, partial=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class FollowViewSet(APIView):
     """Добавление и удаление подписок на автора"""
     serializer_class = FollowSerializer
     permission_classes = (IsAuthenticated,)
     pagination_class = LimitOffsetPagination
-
+    
     def post(self, request, *args, **kwargs):
         user_id = self.kwargs.get('user_id')
         if user_id == request.user.id:
