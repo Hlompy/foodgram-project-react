febddeb06c50aa6525254aa9d79a89579b28dc88
George Kryukov <k.viregor@gmail.com>
2022-06-08 16:12:42 +1100

    admin

--- backend/foodgram/project/admin.py
+++ backend/foodgram/project/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import (Favourites, Ingredients, IngredientAmount, Recipes,
+from .models import (Favorite, Ingredient, IngredientAmount, Recipe,
                      ShoppingCart, Tag)
 
 
@@ -10,7 +10,7 @@ class TagAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-@admin.register(Ingredients)
+@admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'measure')
     list_filter = ('name',)
@@ -18,7 +18,7 @@ class IngredientAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-@admin.register(Recipes)
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'author', 'amount_favorites')
     list_filter = ('author', 'name', 'tags')
@@ -36,7 +36,7 @@ class IngredientAmountAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-@admin.register(Favourites)
+@admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'recipe')
     empty_value_display = '-пусто-'
